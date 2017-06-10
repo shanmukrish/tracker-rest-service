b5aa838a03c2ed157210e41c49961e56ea2a73f0
shanmukrish <shanmuganathankrishnan@live.com>
2017-06-10 07:43:42 +0530

    Initial checkout

---
+++ src/main/java/com/gohiram/haj/trackerrestservice/TrackerRestServiceApplication.java
@@ -0,0 +1,12 @@
+package com.gohiram.haj.trackerrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerRestServiceApplication.class, args);
+	}
+}
